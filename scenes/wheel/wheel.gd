1ca6b44c68c9638cf03220d2c8d9cb4dbba1bd42
RudenkoAD <rudenko.ad@phystech.edu>
2025-03-20 02:43:03 +0300

    fixed a bug i guess

--- scenes/wheel/wheel.gd
+++ scenes/wheel/wheel.gd
@@ -24,8 +24,16 @@ signal puzzle_finished ## emitted when the puzzle is complete.
 
 #region Internal Variables
 var current_value_mappings:Array[int] = [0,60,120,180,240,300]
-enum WheelState {AWAITING_SELECTION,ROTATING,NO_INPUT} ## enum dictating current state of wheel.
+enum WheelState {AWAITING_SELECTION,ROTATING,NO_INPUT,DISABLED} ## enum dictating current state of wheel.
 @export var _state:WheelState = WheelState.NO_INPUT ## variable containing current state of wheel.
+
+var state: WheelState:
+	get:
+		return _state
+	set(value):
+		print("wheel state set to ", value)
+		_state = value
+
 var num_selections:int = 0 ## how many selections have been chosen
 var current_direction:int = 0 ## where the selector currently is.
 var target_selections:int = 6 ## how many selections are allowed; default is 4.
@@ -38,7 +46,7 @@ func _ready()->void:
 	rotation_finished.connect(end_check) # check if puzzle is completed when rotation is done
 # handles input for our minigame
 func _unhandled_input(event: InputEvent) -> void:
-	if _state != WheelState.AWAITING_SELECTION: return
+	if state != WheelState.AWAITING_SELECTION: return
 	if event is InputEventMouseButton:
 		process_confirm_input(current_direction)
 	elif event is InputEventMouseMotion:
@@ -52,23 +60,23 @@ func _unhandled_input(event: InputEvent) -> void:
 
 #region Custom Functions
 func activate():
-	_state = WheelState.AWAITING_SELECTION
+	state = WheelState.AWAITING_SELECTION
 
 
 func deactivate():
-	_state = WheelState.NO_INPUT
+	state = WheelState.DISABLED
 
 
 ## processes the hover direction and moves the selector to that direction.
 func process_direction_input(direction:int)->void:
-	if _state != WheelState.AWAITING_SELECTION: return
+	if state != WheelState.AWAITING_SELECTION: return
 	selector.rotation_degrees = direction #move our selector to the direction
 	new_dir_hovered.emit() # emit signal that we have moved the selector
 	EventBus.update_battle_preview.emit()
 
 ## processes the current selection being clicked
 func process_confirm_input(direction:int)->void:
-	if _state != WheelState.AWAITING_SELECTION: return
+	if state != WheelState.AWAITING_SELECTION: return
 	for x:Control in %covers.get_children():  # show the covers, increase the num selections, emit the signal, rotate
 		if int(round(x.rotation_degrees)) == direction: 
 			if x.visible: return
@@ -80,8 +88,8 @@ func process_confirm_input(direction:int)->void:
 
 ## rotates the slice gimbal +60 degrees
 func rotate_slices(angle: int = 60)->void:
-	#if _state != WheelState.AWAITING_SELECTION: return
-	_state = WheelState.ROTATING 
+	#if state != WheelState.AWAITING_SELECTION: return
+	state = WheelState.ROTATING 
 	current_value_mappings = _rotate_array(current_value_mappings, angle) # +90 to each of our current value mappings
 	rotation_started.emit() 
 	var tween:Tween = create_tween() # create our tween object we will use for the animation
@@ -107,11 +115,12 @@ func reset()->void:
 
 ## checks if the minigame is finished
 func end_check()->void:
+	if state == WheelState.DISABLED: return
 	if num_selections == target_selections:
-		_state = WheelState.NO_INPUT
+		state = WheelState.NO_INPUT
 		puzzle_finished.emit()
 	else:
-		_state = WheelState.AWAITING_SELECTION
+		state = WheelState.AWAITING_SELECTION
 
 func spin():
 	var rng = RandomNumberGenerator.new()
@@ -131,7 +140,6 @@ func set_outer_parts(parts: Array[OuterPart]):
 func get_current_wheel_selection()->WheelSelection:
 	var ws = WheelSelection.new()
 	for x:int in current_value_mappings.size():
-		print(current_value_mappings)
 		if current_direction == current_value_mappings[x]:
 			var slice = slice_gimbal.get_children()[x] as Slice
 			ws.mod = slice.mod
