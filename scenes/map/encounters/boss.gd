cb9af49616c10e4ca3f269b2c13be83edbd1b810
kligunov <kligunov.id@phystech.edu>
2025-03-21 03:04:29 +0300

    improve eye animations

--- scenes/map/encounters/boss.gd
+++ scenes/map/encounters/boss.gd
@@ -2,3 +2,12 @@ extends "res://scenes/map/encounters/encounter.gd"
 
 func encounter():
 	EventBus.request_scene_change.emit(SceneManager.Scene.BATTLE)
+
+func _ready() -> void:
+	stop_animation()
+	$AnimatedSpriteMapLongDistance.play()
+
+func start_animation() -> void:
+	$AnimatedSpriteMapLongDistance.visible = false
+	$AnimatedSprite.visible = true
+	$AnimatedSprite.play()
